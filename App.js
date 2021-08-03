5d03532edff8c5770aa03bc07659ce6828fde4e2
teknixco <vic@teknixco.net>
2021-08-03 00:30:41 +0000

    renamed main data file to MUSIC.JSON and structured code

--- App.js
+++ App.js
@@ -5,18 +5,19 @@
 //BEGIN CODE
 //define constants
 const fs = require('fs');
+const musicFile = 'MUSIC.JSON'
 var music;
 var inputFile='music-import.csv';
 
 //define functions
 const readMusic = () => {
- let rawdata = fs.readFileSync('music.json');
+ let rawdata = fs.readFileSync(musicFile);
  music = JSON.parse(rawdata);
  return music;
 }
 const writeMusic = () => {
  let dataNew = JSON.stringify(music);
- fs.writeFileSync('music.json', dataNew);
+ fs.writeFileSync(musicFile, dataNew);
  return music;
 }
 const addMusic = (artist, song, genre, BPM, speed, mood) => {
@@ -29,4 +30,7 @@ const addMusic = (artist, song, genre, BPM, speed, mood) => {
  music.songs[artist+' - '+song].mood=mood;
  return music;
 }
+
 readMusic();
+//console.log(JSON.stringify(music));
+console.log(music);
