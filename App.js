efa87d44023af393258895eb94bea1a39c4bcf14
teknixco <vic@teknixco.net>
2021-08-06 14:13:50 +0000

    HUGE! Added filterSongs function which can search any property for a value and return the songs!

--- App.js
+++ App.js
@@ -16,12 +16,12 @@ const readMusic = () => {
  let rawdata = fs.readFileSync(musicFile);
  music = JSON.parse(rawdata);
  return music;
-}
+};
 const writeMusic = () => {
  let dataNew = JSON.stringify(music);
  fs.writeFileSync(musicFile, dataNew);
  return music;
-}
+};
 const addMusic = (obj) => {
  const {artist, song, genre, BPM, speed, mood, tags} = obj;
  music.songs[artist+' - '+song]={};  
@@ -33,39 +33,6 @@ const addMusic = (obj) => {
  music.songs[artist+' - '+song].mood=mood;
  music.songs[artist+' - '+song].tags=tags;
  return music;
-}
-const cloneObj = () => {
-// music2 = JSON.parse(JSON.stringify(music))
- music2 = Object.assign(music) //new method
-}
-const getSongs = (obj) => {
- allSongsArr = (obj) => Object.keys(obj.songs);
- return allSongsArr(obj);
-}
-const updateAllSongs = (obj, newkey, newval) => {
- for (song in getSongs(obj)) {obj.songs[(getSongs(obj)[song])][newkey] = newval };
- return obj;
-}
-const getProps = (data, method, func) => {
-  var songsInfo = [];
-  Object.keys(data).forEach(function(prop) {
-    songsInfo.push(data[prop]);
-    if (func === undefined) {
-    //check method for logs
-     if (method == 1) { console.log(data); }
-     else { for (p in data) { console.log(p) }; };
-    }
-    else { songInfo = data[prop]; func();}
-  });
-  //check method for return
-   if (method == 1) { return songsInfo; }
-   else { for (p in data) { return "All values listed"; }; };
- /* Usage:
- getProps(music.songs,1)   //view object
- getProps(music.songs,0)   //view titles
- func = () => {console.log(songInfo)}  // if you need a  function
- getProps(music.songs,0,func)
- */
 };
 const readCSV = async (filePath) => {
   const csvFile = fs.readFileSync(filePath)
@@ -93,6 +60,59 @@ const bulkImportSongs = async () => {
   music = {}; // clear the music object
   music.songs = {}; // create songs object
   let parsedData = await readCSV(importFile);
+};
+const cloneObj = () => {
+// music2 = JSON.parse(JSON.stringify(music))
+ music2 = Object.assign(music) //new method
+}
+const getSongs = (obj) => {
+ allSongsArr = (obj) => Object.keys(obj.songs);
+ return allSongsArr(obj);
+}
+const updateAllSongs = (obj, newkey, newval) => {
+ for (song in getSongs(obj)) {obj.songs[(getSongs(obj)[song])][newkey] = newval };
+ return obj;
+}
+const getProps = (data, method, func) => {
+  var songsInfo = [];
+  Object.keys(data).forEach(function(prop) {
+    songsInfo.push(data[prop]);
+    if (func === undefined) {
+    //check method for logs
+     if (method == 1) { console.log(data); }
+     else { for (p in data) { console.log(p) }; };
+    }
+    else { songInfo = data[prop]; func();}
+  });
+  //check method for return
+  if (method == 1) { return songsInfo; }
+  else { return "All values listed"; };
+  /* Usage:
+  getProps(music.songs,1)   //view object
+  getProps(music.songs,0)   //view titles
+  func = () => {console.log(songInfo)}  // if you need a  function
+  getProps(music.songs,0,func)
+  */
+};
+const filterSongs = (value) => {
+ obj = music.songs; // set object
+ let getPath = [];
+ const getParentAndGrandParent = (getPath, obj, value) => {
+     for (var key in obj) {
+         if (typeof obj[key] === 'object') {
+             getPath.push(key.toString());
+             getParentAndGrandParent(getPath, obj[key], value);
+             getPath.pop();
+         } else {
+             if (obj[key] == value) {
+                 console.log(getPath.toString());
+             };
+         };
+     };
+ };
+ getParentAndGrandParent(getPath, obj, value);
+ /* Usage: filterSongs('any key's value') */
+ return "#djAlgoriddim.V";
 };
 
 
