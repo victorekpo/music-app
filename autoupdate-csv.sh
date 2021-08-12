cc48192c6c54525df92ae660b9a435c87bd8d1e7
teknixco <vic@teknixco.net>
2021-08-12 12:59:58 +0000

    Added import to autoupdate script

--- autoupdate-csv.sh
+++ autoupdate-csv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+dir="/home/ubuntu/projects/music/music-app"
 DOCUMENT="1IcBU5LJmLSmIQe1j0TpJnQKDcw8lnVzrbCXcy8BpoMQ"
 SHEET="1109832368"
-FILE="/home/ubuntu/projects/music/music-app/MUSIC.CSV"
+FILE="$dir/MUSIC.CSV"
 wget "https://docs.google.com/spreadsheets/d/$DOCUMENT/export?format=csv&gid=$SHEET" -O $FILE
 dos2unix $FILE
+node $dir/App.js import
