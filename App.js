61ff1f7ff2353ecdf8ad2267c786692e4824ef13
teknixco <vic@teknixco.net>
2021-08-16 05:10:30 +0000

    added color to output for better visualization of search results

--- App.js
+++ App.js
@@ -53,16 +53,16 @@ const writeMusic = () => {
 };
 const addMusic = (obj) => {
  const {artist, song, album, genre, BPM, speed, mood, tags, quotes} = obj;
- music.songs[artist+' - '+song]={};  
- music.songs[artist+' - '+song].artist=artist;
- music.songs[artist+' - '+song].song=song;
- music.songs[artist+' - '+song].album=album;
- music.songs[artist+' - '+song].genre=genre;
- music.songs[artist+' - '+song].BPM=BPM;
- music.songs[artist+' - '+song].speed=speed;
- music.songs[artist+' - '+song].mood=mood;
- music.songs[artist+' - '+song].tags=tags;
- music.songs[artist+' - '+song].quotes=quotes;
+ music.songs[artist+' -- '+song]={};  
+ music.songs[artist+' -- '+song].artist=artist;
+ music.songs[artist+' -- '+song].song=song;
+ music.songs[artist+' -- '+song].album=album;
+ music.songs[artist+' -- '+song].genre=genre;
+ music.songs[artist+' -- '+song].BPM=BPM;
+ music.songs[artist+' -- '+song].speed=speed;
+ music.songs[artist+' -- '+song].mood=mood;
+ music.songs[artist+' -- '+song].tags=tags;
+ music.songs[artist+' -- '+song].quotes=quotes;
  return music;
 };
 const readCSV = async (filePath, mode) => {
