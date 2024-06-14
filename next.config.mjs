957d590e765797e09800a7b504bc8c193d7b8d92
Victor Ekpo <victor.ekpo@gmail.com>
2024-06-14 05:36:14 -0500

    refactor: update transformjstoenv plugin

--- next.config.mjs
+++ next.config.mjs
@@ -26,93 +26,87 @@ class TransformJsToEnvPlugin {
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tapAsync("TransformJsToEnvPlugin", (compilation, callback) => {
-      const dirPath = path.resolve(this.options.sourceDir);
-      const outputDir = path.resolve(this.options.outputDir);
-
-      fs.readdir(dirPath, async (err, files) => {
-        if (err) {
-          compilation.errors.push(err);
-          callback();
-          return;
-        }
-
-        const defaultConfigPath = path.join(dirPath, "default.js");
-
-        console.log("DEFAULT CONFIG PATH", defaultConfigPath);
-
-        // Read default configuration
-        const defaultConfig = await this.loadConfig(defaultConfigPath);
-        console.log("DEFAULT CONFIG", defaultConfig);
-
-        if (!defaultConfig) {
-          compilation.errors.push(new Error("Default configuration file not found or invalid."));
-          callback();
-          return;
-        }
-
-        files.forEach(file => {
-          (async () => {
-            const filePath = path.join(dirPath, file);
-            if (file !== "default.js" && path.extname(file) === ".js") {
-              const envConfig = await this.loadConfig(filePath);
-
-              if (envConfig) {
-                const mergedConfig = {...defaultConfig, ...envConfig};
-                console.log("MERGED CONFIG", mergedConfig);
-                const envContent = this.transformJsToEnv(mergedConfig);
-
-                const envFileName = `.env.${path.basename(file, ".js")}`;
-                const envFilePath = path.join(outputDir, envFileName);
-
-                fs.writeFile(envFilePath, envContent, "utf8", (err) => {
-                  console.log("WRITING FILE");
-                  if (err) {
-                    compilation.errors.push(err);
-                  }
-                });
-              }
-            }
-          })();
-        });
-
+    compiler.hooks.emit.tapAsync('TransformJsToEnvPlugin', async (compilation, callback) => {
+      try {
+        await this.generateEnvFiles();
         callback();
-      });
+      } catch (error) {
+        compilation.errors.push(error);
+        callback();
+      }
     });
   }
 
+  async generateEnvFiles() {
+    const dirPath = path.resolve(this.options.sourceDir);
+    const outputDir = path.resolve(this.options.outputDir);
+
+    const files = await fs.promises.readdir(dirPath);
+    const defaultConfigPath = path.join(dirPath, 'default.js');
+    console.log('DEFAULT CONFIG PATH', defaultConfigPath);
+
+    const defaultConfig = await this.loadConfig(defaultConfigPath);
+    console.log('DEFAULT CONFIG', defaultConfig);
+
+    if (!defaultConfig) {
+      throw new Error('Default configuration file not found or invalid.');
+    }
+
+    await Promise.all(files.map(file => this.processFile(file, dirPath, outputDir, defaultConfig)));
+  }
+
+  async processFile(file, dirPath, outputDir, defaultConfig) {
+    const filePath = path.join(dirPath, file);
+    if (file !== 'default.js' && path.extname(file) === '.js') {
+      const envConfig = await this.loadConfig(filePath);
+
+      if (envConfig) {
+        const mergedConfig = { ...defaultConfig, ...envConfig };
+        console.log('MERGED CONFIG', mergedConfig);
+
+        const envContent = this.transformJsToEnv(mergedConfig);
+        const envFileName = `.env.${path.basename(file, '.js')}`;
+        const envFilePath = path.join(outputDir, envFileName);
+
+        await fs.promises.writeFile(envFilePath, envContent, 'utf8');
+        console.log('WRITING FILE', envFilePath);
+      }
+    }
+  }
+
   async loadConfig(filePath) {
-    console.log("LOADING CONFIG", filePath);
+    console.log('LOADING CONFIG', filePath);
     try {
       const configModule = await import(filePath);
       const config = configModule.default;
-      console.log("CONFIG", config.default);
+      console.log('CONFIG', config);
 
-      if (typeof config === "object" && config !== null) {
+      if (typeof config === 'object' && config !== null) {
         return config;
       }
     } catch (e) {
+      console.error('Error loading config', e);
       return null;
     }
   }
 
-  transformJsToEnv(config, prefix) {
-    console.log("CONFIG TO TRANSFORM", config);
+  transformJsToEnv(config, prefix = '') {
+    console.log('CONFIG TO TRANSFORM', config);
     const objToEntries = Object.entries(config);
-    console.log("OBJ TO ENTRIES", objToEntries);
+    console.log('OBJ TO ENTRIES', objToEntries);
+
     return objToEntries
       .map(([key, value]) => {
-        let valueToReturn=value;
-        if (typeof value === "object") {
-          console.log("VALUE IS OBJ", value);
-          const addPrefix = prefix ? prefix + '_' + key : key;
-          const valueToReturn = this.transformJsToEnv(value, addPrefix);
-          console.log("OBJVALUERETURN", valueToReturn);
-          return valueToReturn;
+        if (typeof value === 'object' && value !== null) {
+          console.log('VALUE IS OBJ', value);
+          const newPrefix = prefix ? `${prefix}_${key}` : key;
+          const nestedEnv = this.transformJsToEnv(value, newPrefix);
+          console.log('NESTED ENV', nestedEnv);
+          return nestedEnv;
         }
-        return `${prefix ? prefix + '_' + key : '' + key}=${valueToReturn}`;
+        return `${prefix ? `${prefix}_` : ''}${key}=${value}`;
       })
-      .join("\n");
+      .join('\n');
   }
 }
 
