bb28c023f6ef4e9bdcc60e65ceb5339479dab885
Victor Ekpo <victor.ekpo@gmail.com>
2024-06-15 11:21:38 -0500

    remove ifServer

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,23 @@
 /** @type {import("next").NextConfig} */
 import path from "path";
-import TransformConfigToEnvPlugin from './webpack/plugins/TransformConfigToEnvPlugin.js';
+import TransformConfigToEnvPlugin from "./webpack/plugins/TransformConfigToEnvPlugin.js";
 
 const nextConfig = {
-  webpack: (config, {isServer}) => {
-    // // Add custom plugin only on the server side build
-     if (isServer) {
-      config.plugins.push(
-        new TransformConfigToEnvPlugin({
-          sourceDir: path.resolve(".", "config"), // Path to your config directory
-          outputDir: path.resolve(".", "."), // Path where .env files should be generated
-          configMappings: {
-            dev: 'development',
-            int: 'development',
-            qa: 'production',
-            prod: 'production',
-            test: 'test'
-          },
-          fileExt: '.js'
-        })
-      );
-     }
+  webpack: (config) => {
+    config.plugins.push(
+      new TransformConfigToEnvPlugin({
+        sourceDir: path.resolve(".", "config"), // Path to your config directory
+        outputDir: path.resolve(".", "."), // Path where .env files should be generated
+        configMappings: {
+          dev: "development",
+          int: "development",
+          qa: "production",
+          prod: "production",
+          test: "test"
+        },
+        fileExt: ".js"
+      })
+    );
     return config;
   }
 };
