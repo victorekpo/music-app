df797aeaec5f8a6d3112c79e505479ea20e2fd41
Victor Ekpo <victor.ekpo@gmail.com>
2024-06-16 00:29:25 -0500

    add tag route

--- src/app/api/v1/get/route.ts
+++ src/app/api/v1/get/route.ts
@@ -1,12 +1,7 @@
 import { music, readMusic } from "@/utils/music/read";
-
-const { defaultPort } = process.env;
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = (req: NextRequest) => {
-  // const { headers, url } = req;
-
   readMusic();
-  // console.log(music);
   return NextResponse.json(music);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-const { defaultPort } = process.env;
-
 const HomePage = () => {
-  console.log("PAGE ENV", defaultPort)
   return (
     <>
       <div className=''>HomePage</div>
