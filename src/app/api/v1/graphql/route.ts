907dfdc126fcea51875fc780b2305d10bf830bdd
Vic <victor.ekpo@gmail.com>
2024-06-22 13:26:19 -0500

    update graphql server

--- src/app/api/v1/graphql/route.ts
+++ src/app/api/v1/graphql/route.ts
@@ -1,14 +1,7 @@
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { ApolloServer } from "@apollo/server";
 import { NextRequest } from "next/server";
-import { resolvers } from "@/graphql/server/resolvers";
-import { typeDefs } from "@/graphql/server/typeDefs";
 import { contextMiddleware } from "@/graphql/server/middleware";
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+import { server } from "@/graphql/server/server";
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
   context: contextMiddleware,
