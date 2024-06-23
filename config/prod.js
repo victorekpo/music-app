39373939748d4c7c2e60b05fe35e15a97e6cb6b0
Vic <victor.ekpo@gmail.com>
2024-06-23 01:06:32 -0500

    update prod graphql

--- config/prod.js
+++ config/prod.js
@@ -2,10 +2,10 @@ const defaultPort = 3000;
 
 module.exports = {
   defaultPort,
-  graphQLServer: `http://music.teknixco.net:${defaultPort}/api/v1/graphql`,
+  graphQLServer: `http://music.teknixco.net/api/v1/graphql`,
   NEXT: {
     PUBLIC: {
-      graphQLServer: `http://music.teknixco.net:${defaultPort}/api/v1/graphql`,
+      graphQLServer: `http://music.teknixco.net/api/v1/graphql`,
     }
   }
 }
