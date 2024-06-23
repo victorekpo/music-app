e5881801e324abf755cbc1004916a7cb1c868df2
Vic <victor.ekpo@gmail.com>
2024-06-23 01:19:43 -0500

    add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
     <ApolloWrapper>
        <AppContextProvider>
         <html lang="en">
+          <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+          </head>
           <body className="container">
           <Navbar />
           <div className={style.pageContainer}>
