ddbfe4b86ef42a278618efadb99a1a87a2045ee3
Victor Ekpo <victor.ekpo@gmail.com>
2024-06-25 19:34:09 -0500

    add input boxes for add page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,9 +1,45 @@
+'use client'
+
+import { Button, Input } from "@nextui-org/react";
+import { useState } from "react";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target.values);
+}
 const AddMusicPage = () => {
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+  const [album, setAlbum] = useState('');
+  const [genre, setGenre] = useState('');
+  const [BPM, setBPM] = useState('');
+  const [speed, setSpeed] = useState('');
+  const [mood, setMood] = useState('');
+  const [tags, setTags] = useState(''); // Todo: Change to array
+  const [quotes, setQuotes]=useState(''); // Todo: Change to array
   return (
     <>
       <div className=''>Add Music Page</div>
-    </>
-  )
+      <form onSubmit={ handleSubmit }>
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+          <Input
+            type="Artist"
+            label="Artist"
+            required
+          />
+          <Input
+            type="Song"
+            label="Song"
+            required
+          />
+          <Input/>
+          <Input/>
+          <Button type="submit" color="primary">Submit</Button>
+        </div>
+      </form>
+
+</>
+)
 }
 
 export default AddMusicPage
